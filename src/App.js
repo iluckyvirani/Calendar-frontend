90ffb76711bdedd186b20b314886e65edbccf36e
Shivangi889 <90382744+iluckyvirani@users.noreply.github.com>
2025-03-09 20:20:43 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ const App = () => {
 
     const fetchEvents = async () => {
         try {
-            const response = await axios.get(`http://localhost:5000/api/events`);
+            const response = await axios.get(`${baseurl}api/events`);
             const formattedEvents = response.data.map((event) => ({
                 id: event._id,
                 title: event.title,
